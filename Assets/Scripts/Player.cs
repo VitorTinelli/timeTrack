dde03b317f3f5d1f6edcec4582ba0770d442661e
Lucas De Bitencourt Frasson <lucas.260617@alunosatc.edu.br>
2023-06-14 16:23:33 -0300

    rapaiz

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine; 
+
+public class Player : MonoBehaviour
+{
+    public Transform gravityTarget;
+
+    // Contador
+    private bool canMove = false;
+    private int countdownTime = 0;
+
+    // Carro
+    public float power = 15000f;
+    public float torque = 500f;
+    public float gravity = 9.81f;
+
+    public bool autoOrient = false;
+    public float autoOrientSpeed = 1f;
+
+    private float horInput;
+    private float verInput;
+    private float steerAngle;
+
+     public Wheel[] wheels;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        StartCoroutine(CountdownToStart());
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() 
+    {
+        ProcessInput();
+        Vector3 diff = transform.position - gravityTarget.position;
+        if(autoOrient) { AutoOrient(-diff); }
+    }
+
+    void FixedUpdate()
+    {
+        ProcessForces();
+        ProcessGravity();
+    }
+
+    void ProcessInput()
+    {
+        if (canMove)
+        {
+            verInput = (Input.GetAxis("Vertical")) * -1;
+            horInput = Input.GetAxis("Horizontal");
+        }
+    }
+    void ProcessForces()
+    {
+        //Vector3 force = new Vector3(0f, 0f, verInput * power);
+        //rb.AddRelativeForce(force);
+
+        //Vector3 rforce = new Vector3(0f, horInput* torque, 0f);
+        //rb.AddRelativeTorque(rforce);
+
+        foreach(Wheel w in wheels)
+        {
+            w.Steer(horInput);
+            w.Accelerate(verInput * power);
+            w.UpdatePosition();
+        }
+    }
+
+    void ProcessGravity()
+    {
+        Vector3 diff = transform.position - gravityTarget.position;
+        rb.AddForce(-diff.normalized * gravity * (rb.mass));
+    }
+
+    void AutoOrient(Vector3 down)
+    {
+        Quaternion orientationDirection = Quaternion.FromToRotation(-transform.up, down) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, orientationDirection, autoOrientSpeed * Time.deltaTime);
+    }
+    IEnumerator CountdownToStart()
+    {
+        while (countdownTime > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            countdownTime--;
+            Console.WriteLine(canMove);
+        }
+        canMove = true;
+    }
+}
